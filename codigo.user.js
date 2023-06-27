ed23202f6b55acb06950f4ec16c195a2484a71e2
Lucas Gabriel <66486119+lucasgabriel07@users.noreply.github.com>
2023-06-27 00:51:09 -0300

    Ajustando para os horários da UFMA

--- codigo.user.js
+++ codigo.user.js
@@ -6,7 +6,7 @@
 // @description  Traduz as informações de horários das turmas no SIGAA (novo sistema da UnB), de formato pouco entendível, por dias e horas escritas por extenso.
 // @author       Luthiery Costa
 // @supportURL   https://github.com/luthierycosta
-// @match        https://sigaa.unb.br/sigaa/*
+// @match        https://sigaa.ufma.br/sigaa/*
 // @grant        none
 // @noframes
 // ==/UserScript==
@@ -23,22 +23,22 @@ const mapaDias = {
     7: 'SAB'
 }
 const mapaHorarios = {
-    'M1': {inicio: '08:00', fim: '08:55'},
-    'M2': {inicio: '08:55', fim: '09:50'},
-    'M3': {inicio: '10:00', fim: '10:55'},
-    'M4': {inicio: '10:55', fim: '11:50'},
-    'M5': {inicio: '12:00', fim: '12:55'},
-    'T1': {inicio: '12:55', fim: '13:50'},
-    'T2': {inicio: '14:00', fim: '14:55'},
-    'T3': {inicio: '14:55', fim: '15:50'},
-    'T4': {inicio: '16:00', fim: '16:55'},
-    'T5': {inicio: '16:55', fim: '17:50'},
-    'T6': {inicio: '18:00', fim: '18:55'},
-    'T7': {inicio: '18:55', fim: '19:50'},
-    'N1': {inicio: '19:00', fim: '19:50'},
-    'N2': {inicio: '19:50', fim: '20:40'},
-    'N3': {inicio: '20:50', fim: '21:40'},
-    'N4': {inicio: '21:40', fim: '22:30'}
+    'M1': {inicio: '07:30', fim: '08:20'},
+    'M2': {inicio: '08:20', fim: '09:10'},
+    'M3': {inicio: '09:20', fim: '10:10'},
+    'M4': {inicio: '10:10', fim: '11:00'},
+    'M5': {inicio: '11:10', fim: '12:00'},
+    'M6': {inicio: '12:00', fim: '12:50'},
+    'T1': {inicio: '13:10', fim: '14:00'},
+    'T2': {inicio: '14:00', fim: '14:50'},
+    'T3': {inicio: '14:50', fim: '15:40'},
+    'T4': {inicio: '15:50', fim: '16:40'},
+    'T5': {inicio: '16:40', fim: '17:30'},
+    'T6': {inicio: '17:40', fim: '18:30'},
+    'N1': {inicio: '18:30', fim: '19:20'},
+    'N2': {inicio: '19:20', fim: '20:10'},
+    'N3': {inicio: '20:20', fim: '21:10'},
+    'N4': {inicio: '21:10', fim: '22:00'}
 }
 
 /** Padrão regex que reconhece o formato de horário do SIGAA */
